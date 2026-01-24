f8697d18670849b401cb031388121936c885fa03
andrei-mushovets <andreimyshoves89@gmail.com>
2026-01-24 18:48:39 +0100

    repair revewier issues

--- client/src/app/router/rootRoute.tsx
+++ client/src/app/router/rootRoute.tsx
@@ -4,18 +4,17 @@ import styles from './RootLayout.module.css'
 
 export const rootRoute = createRootRoute({
     component: () => (
-        <>
-            <header>
-                <Link to="/">Catalog</Link> <Link to="/about">About</Link>
+        <div className={styles.root}>
+            <header className={styles.header}>
+                <Link to="/">Catalog</Link>
+                <Link to="/about">About</Link>
             </header>
 
-            <div className={styles.root}>
-                <main className={styles.container}>
-                    <Outlet />
-                </main>
-            </div>
+            <main className={styles.container}>
+                <Outlet />
+            </main>
 
             <TanStackRouterDevtools />
-        </>
+        </div>
     ),
 })
