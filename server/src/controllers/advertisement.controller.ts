365e0407cc7103b010257d83e6cfa83394ff9959
andrei-mushovets <andreimyshoves89@gmail.com>
2026-01-29 15:37:07 +0100

    test endpoint advertisement

--- server/src/controllers/advertisement.controller.ts
+++ server/src/controllers/advertisement.controller.ts
@@ -1,48 +1,16 @@
-import { Request, Response } from 'express'
-import Advertisement from '../models/Advertisement'
-import { ApiError } from '../error/ApiError'
-import path from 'path'
-import { v4 as uuidv4 } from 'uuid'
+import { Request, Response, NextFunction } from 'express'
+import { ApiError } from '../error/ApiError.js'
 
-export const createAdvertisement = async (req: Request, res: Response) => {
-    const { name, description, price } = req.body
+export const createAdvertisement = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { name, description, price } = req.body
 
-    if (!name || !description || !price) {
-        throw ApiError.badRequest('name, description and price are required')
-    }
-
-    if (!req.user) {
-        throw ApiError.unauthorized()
-    }
-
-    let images: string[] | undefined = undefined
-
-    const filesMap = req.files as { [key: string]: any } | undefined
-
-    if (filesMap?.images) {
-        const files = Array.isArray(filesMap.images) ? filesMap.images : [filesMap.images]
-
-        images = []
-
-        for (const file of files) {
-            const fileName = `${uuidv4()}.jpg`
-            const filePath = path.resolve(__dirname, '..', 'static', fileName)
-
-            await file.mv(filePath)
-            images.push(fileName)
+        if (!name || !description || !price) {
+            return next(ApiError.badRequest('name, description and price are required'))
         }
+
+        return res.json({ success: true })
+    } catch (err) {
+        next(err)
     }
-
-    const advertisement = await Advertisement.create({
-        name,
-        description,
-        price: Number(price),
-        images,
-        userId: Number(req.user.userId),
-    })
-
-    res.status(201).json({
-        success: true,
-        advertisement,
-    })
 }
