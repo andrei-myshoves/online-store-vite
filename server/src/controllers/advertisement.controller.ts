acad36209804ff0e48db62db2e1705153dac2a57
andrei-mushovets <andreimyshoves89@gmail.com>
2026-02-04 17:17:38 +0100

    add validate for post

--- server/src/controllers/advertisement.controller.ts
+++ server/src/controllers/advertisement.controller.ts
@@ -3,21 +3,17 @@ import { ApiError } from '../error/ApiError.js'
 import Advertisement from '@/models/Advertisement.js'
 
 export const createAdvertisement = async (req: Request, res: Response) => {
-    const { name, description, price } = req.body
-
-    if (!name || !description || !price) {
-        throw ApiError.badRequest('name, description and price are required')
-    }
-
     if (!req.user) {
         throw ApiError.unauthorized()
     }
 
+    const { name, description, price } = req.body
+
     const advertisement = await Advertisement.create({
         name,
         description,
-        price: Number(price),
-        userId: 1,
+        price,
+        userId: req.user.userId,
     })
 
     res.status(201).json(advertisement)
