b22a55c0868bc6faade49e595886c46b39f95217
andrei-mushovets <andreimyshoves89@gmail.com>
2026-02-07 00:04:02 +0100

    add pagination for advertisement

--- client/src/pages/CatalogPage.tsx
+++ client/src/pages/CatalogPage.tsx
@@ -1,32 +1,68 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState, useCallback } from 'react'
 import styles from './CatalogPage.module.css'
 import { api } from '@/shared/api/api'
 import { AdvList } from '@/widgets/adv-list/AdvList'
 import type { Advertisement } from '@/entities/advertisement/models/types'
 
 export const CatalogPage = () => {
+    const LIMIT = 10
+
     const [items, setItems] = useState<Advertisement[]>([])
-    const [loading, setLoading] = useState(true)
+    const [offset, setOffset] = useState(0)
+    const [loading, setLoading] = useState(false)
     const [error, setError] = useState<string | null>(null)
+    const [hasMore, setHasMore] = useState(true)
+
+    const loaderRef = useRef<HTMLDivElement | null>(null)
+
+    const loadAdvertisements = useCallback(async () => {
+        if (loading || !hasMore) return
+
+        try {
+            setLoading(true)
+            const { data } = await api.get('/advertisement', {
+                params: { limit: LIMIT, offset },
+            })
+
+            setItems(prev => [...prev, ...data.items])
+            setOffset(prevOffset => {
+                const newOffset = prevOffset + data.items.length
+                setHasMore(newOffset < data.total)
+                return newOffset
+            })
+        } catch {
+            setError('Ошибка загрузки')
+        } finally {
+            setLoading(false)
+        }
+    }, [loading, hasMore, offset])
 
     useEffect(() => {
-        const load = async () => {
-            try {
-                const { data } = await api.get('/advertisement')
-                setItems(data.items)
-            } catch {
-                setError('Ошибка загрузки объявлений')
-            } finally {
-                setLoading(false)
-            }
+        const observer = new IntersectionObserver(
+            entries => {
+                if (entries[0].isIntersecting && hasMore && !loading) {
+                    loadAdvertisements()
+                }
+            },
+            { threshold: 0.1 }
+        )
+
+        if (loaderRef.current) {
+            observer.observe(loaderRef.current)
         }
 
-        load()
-    }, [])
+        return () => observer.disconnect()
+    }, [loadAdvertisements, hasMore, loading])
 
     return (
         <div className={styles.page}>
             <AdvList items={items} loading={loading} error={error} />
+
+            {hasMore && (
+                <div ref={loaderRef} style={{ height: '50px', margin: '20px 0' }}>
+                    {loading && <p>Загрузка новых объявлений...</p>}
+                </div>
+            )}
         </div>
     )
 }
