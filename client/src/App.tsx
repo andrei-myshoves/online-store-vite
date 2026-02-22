932f167e3c6492d925afb8283a2dfce6ac88dbea
andrei-mushovets <andreimyshoves89@gmail.com>
2026-02-22 18:57:18 +0100

    move Redux Provider inside App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,14 @@
 import { RouterProvider } from '@tanstack/react-router'
 import { router } from '@/app/router/router'
+import { Provider } from 'react-redux'
+import { setupStore } from '@/store/store'
+
+const store = setupStore()
 
 export const App = () => {
-    return <RouterProvider router={router} />
+    return (
+        <Provider store={store}>
+            <RouterProvider router={router} />
+        </Provider>
+    )
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,18 +1,12 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
 import { App } from './App'
-import { setupStore } from '@/store/store'
 import './style.css'
-
-const store = setupStore()
 
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 
 root.render(
     <StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
+        <App />
     </StrictMode>
 )
