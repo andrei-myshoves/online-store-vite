b1c471a9cf7330c4c913c439246bec06412e8eec
andrei-mushovets <andreimyshoves89@gmail.com>
2026-03-06 23:17:36 +0100

    add endpoint for page user

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,6 +3,7 @@ import authRoutes from './auth.routes.js'
 import advertisementRoutes from './advertisement.routes.js'
 import reviewRouter from './review.routes.js'
 import sellerRouter from './seller.routes.js'
+import userRoutes from './user.routes.js'
 
 const router = Router()
 
@@ -10,5 +11,6 @@ router.use('/auth', authRoutes)
 router.use('/advertisement', advertisementRoutes)
 router.use('/reviews', reviewRouter)
 router.use('/seller', sellerRouter)
+router.use('/users', userRoutes)
 
 export default router
