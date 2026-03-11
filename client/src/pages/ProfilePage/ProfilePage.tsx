dc93eb24150efa8764339c8a274aa484c4794ac4
andrei-mushovets <andreimyshoves89@gmail.com>
2026-03-11 23:30:55 +0100

    resolv reviewer issues

--- client/src/pages/ProfilePage/ProfilePage.tsx
+++ client/src/pages/ProfilePage/ProfilePage.tsx
@@ -1,19 +1,18 @@
 import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useAppDispatch, useAppSelector } from '@/hooks/redux'
 import { AdvertisementTopBar } from '@/shared/ui/AdvertisementTopBar/AdvertisementTopBar'
 import { ProfileSettings } from '@/widgets/ProfileSettings/ProfileSettings'
 import { AdvertisementsList } from '@/widgets/AdvertisementsList/AdvertisementsList'
 import { fetchProfile } from '@/store/reducers/profile/profileThunks'
 import { selectProfile, selectProfileLoading, selectProfileError } from '@/store/reducers/selectors/profileSelectors'
-import type { AppDispatch } from '@/store/store'
 import styles from './ProfilePage.module.css'
 
 export const ProfilePage = () => {
-    const dispatch = useDispatch<AppDispatch>()
+    const dispatch = useAppDispatch()
 
-    const profile = useSelector(selectProfile)
-    const isLoading = useSelector(selectProfileLoading)
-    const error = useSelector(selectProfileError)
+    const profile = useAppSelector(selectProfile)
+    const isLoading = useAppSelector(selectProfileLoading)
+    const error = useAppSelector(selectProfileError)
 
     useEffect(() => {
         dispatch(fetchProfile())
--- client/src/widgets/ProfileSettings/ProfileSettings.tsx
+++ client/src/widgets/ProfileSettings/ProfileSettings.tsx
@@ -1,16 +1,31 @@
+import { useState, useEffect } from 'react'
 import { Input } from '@/shared/ui/input'
 import { Button } from '@/shared/ui/button'
-import styles from './ProfileSettings.module.css'
 import type { Profile } from '@/entities/advertisement/models/types'
+import styles from './ProfileSettings.module.css'
 
 type Props = {
     profile: Profile | null
 }
 
 export const ProfileSettings = ({ profile }: Props) => {
+    const [username, setUsername] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [city, setCity] = useState('')
+    const [phone, setPhone] = useState('')
+
+    useEffect(() => {
+        if (profile) {
+            setUsername(profile.username ?? '')
+            setLastName(profile.lastName ?? '')
+            setCity(profile.city ?? '')
+            setPhone(profile.phone ?? '')
+        }
+    }, [profile])
+
     return (
         <section className={styles.wrapper}>
-            <h1 className={styles.greeting}>Здравствуйте, {profile?.username ?? 'пользователь'}!</h1>
+            <h1 className={styles.greeting}>Здравствуйте, {username}!</h1>
 
             <h2 className={styles.title}>Настройки профиля</h2>
 
@@ -21,13 +36,23 @@ export const ProfileSettings = ({ profile }: Props) => {
                 </div>
 
                 <div className={styles.form}>
-                    <Input placeholder="Имя" defaultValue={profile?.username ?? ''} />
+                    <Input placeholder="Имя" value={username} onChange={e => setUsername(e.target.value)} />
 
-                    <Input placeholder="Фамилия" defaultValue={profile?.lastName ?? ''} />
+                    <Input placeholder="Фамилия" value={lastName} onChange={e => setLastName(e.target.value)} />
 
-                    <Input className={styles.city} placeholder="Город" defaultValue={profile?.city ?? ''} />
+                    <Input
+                        className={styles.city}
+                        placeholder="Город"
+                        value={city}
+                        onChange={e => setCity(e.target.value)}
+                    />
 
-                    <Input className={styles.phone} placeholder="Телефон" defaultValue={profile?.phone ?? ''} />
+                    <Input
+                        className={styles.phone}
+                        placeholder="Телефон"
+                        value={phone}
+                        onChange={e => setPhone(e.target.value)}
+                    />
 
                     <Button className={styles.saveButton}>Сохранить</Button>
                 </div>
